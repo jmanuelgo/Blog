8a86fa98f88ced280b76d729c8c004c359c16d0d
jmanuelgo <gandamanu330@gmail.com>
2024-10-03 10:19:05 -0400

    Error en las rutas

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,24 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Routing\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        using: function(illuminate\Routing\Router $router){
+           $router-> middleware('api')
+                    ->prefix('api')
+                    ->group(base_path('routes/api.php'));
+            $router->middleware('web')
+                    ->prefix('web')
+                    ->group(base_path('routes/web.php'));
+            $router->middleware('web')
+                    ->prefix('admin')
+                    ->group(base_path('routes/admin.php'));
+
+        }
+        
+            
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
